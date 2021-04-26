7a800a539dd2ab9f3fee6985f6c3f5f6760706b1
archie-swif <artem.ryabokon@gmail.com>
2021-04-26 16:13:19 -0700

    Debugging

--- src/test_display.py
+++ src/test_display.py
@@ -27,11 +27,10 @@ def test_show_image_on_software():
     display = Display()
 
     image = Image.new('RGB', (800, 480), (255, 255, 255))  # 255: clear the frame
-    draw_Himage = ImageDraw.Draw(image)
-    draw_Himage.line((140, 75, 190, 75), fill=0)
-    draw_Himage.arc((140, 50, 190, 100), 0, 360, fill=0)
-    draw_Himage.rectangle((80, 50, 130, 100), fill=(0, 0, 0))
-    draw_Himage.chord((200, 50, 250, 100), 0, 360, fill=(255, 0, 0))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("img/Perfect DOS VGA 437.ttf", 40)
+    draw.text((10, 10), "Hello", font=font, fill=(255, 0, 0))
+    draw.text((10, 50), "WORLD", font=font, fill=(0, 0, 0))
     display.show_on_software(image)
 
 
